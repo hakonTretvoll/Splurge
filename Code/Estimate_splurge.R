87eed4cf906ab64238b845096f4876b1f9aefccd
edmundcrawley <edmundcrawley@gmail.com>
2020-04-23 22:26:58 -0400

    Bug fixes and targeting particular moments

--- Code/Estimate_splurge.R
+++ Code/Estimate_splurge.R
@@ -2,6 +2,7 @@
 
 require(numDeriv)
 require(MCMCpack)
+require(ks)
 
 #This is the covariance of two stochastic processes, driven by the same underlying shocks, 
 #but which decay at different exponential rates omega and theta
@@ -11,33 +12,33 @@ cov_omega_theta <-function(omega, theta){
   omth = omega + theta
   omthx = 1.0/(1.0-exp(-omth))
 
-  var = 2.0*thetax*omegax 
-        + thetax*omegax*( 
+  var = 2.0*thetax*omegax +
+         thetax*omegax*( 
                  ((2.0-exp(-theta))*(2.0-exp(-omega))+1.0)/(omth*omthx) 
                  - (3.0-exp(-theta))/(theta*thetax) - (3.0-exp(-omega))/(omega*omegax)  
-                  ) 
-        + 1.0/(omth*omegax*thetax)
+                  ) +
+         1.0/(omth*omegax*thetax)
   # Covariance for moving the theta process up to T+1
   cov_1 = thetax*omegax*((2.0-exp(-theta))/(theta*thetax) 
                        + 1.0/(omega*omegax) - (2.0-exp(-theta))/(omth*omthx) 
-                       -1.0 ) 
-          - omegax/thetax*((2.0-exp(-omega))/(omth*omthx) - 1.0/(theta*thetax) ) 
-          + exp(-theta)/(omegax*thetax*omth)
+                       -1.0 ) -
+           omegax/thetax*((2.0-exp(-omega))/(omth*omthx) - 1.0/(theta*thetax) ) +
+           exp(-theta)/(omegax*thetax*omth)
   # Covariance for moving the theta process up to T+2
-  cov_2 = -omegax/thetax * (1.0/(theta*thetax) - 1.0/(omth*omthx)) 
-         -omegax/thetax * (exp(-theta)*(2.0-exp(-omega))/(omth*omthx) - exp(-theta)/(theta*thetax) ) 
-         + exp(-2.0*theta)/(omegax*thetax*omth)
+  cov_2 = -omegax/thetax * (1.0/(theta*thetax) - 1.0/(omth*omthx)) -
+         omegax/thetax * (exp(-theta)*(2.0-exp(-omega))/(omth*omthx) - exp(-theta)/(theta*thetax) ) +
+         exp(-2.0*theta)/(omegax*thetax*omth)
 #cov_M = exp(-(M-2.0)*theta)*cov2
   # Covariance for moving the omega process up to T+1
-  cov_m1 = thetax*omegax*((2.0-exp(-omega))/(omega*omegax) 
-                       + 1.0/(theta*thetax) - (2.0-exp(-omega))/(omth*omthx)
-                       -1.0 ) 
-          - thetax/omegax*((2.0-exp(-theta))/(omth*omthx) - 1.0/(omega*omegax) ) 
-          + exp(-omega)/(omegax*thetax*omth)
+  cov_m1 = thetax*omegax*((2.0-exp(-omega))/(omega*omegax) +
+                       1.0/(theta*thetax) - (2.0-exp(-omega))/(omth*omthx)
+                       -1.0 ) -
+          thetax/omegax*((2.0-exp(-theta))/(omth*omthx) - 1.0/(omega*omegax) ) +
+          exp(-omega)/(omegax*thetax*omth)
   # Covariance for moving the omega process up to T+2
-  cov_m2 = -thetax/omegax * (1.0/(omega*omegax) - 1.0/(omth*omthx)) 
-                -thetax/omegax * (exp(-omega)*(2.0-exp(-theta))/(omth*omthx) - exp(-omega)/(omega*omegax) ) 
-                + exp(-2.0*omega)/(omegax*thetax*omth)
+  cov_m2 = -thetax/omegax * (1.0/(omega*omegax) - 1.0/(omth*omthx)) -
+                thetax/omegax * (exp(-omega)*(2.0-exp(-theta))/(omth*omthx) - exp(-omega)/(omega*omegax) ) +
+                exp(-2.0*omega)/(omegax*thetax*omth)
 #cov_mM = exp((2.0+M)*omega)*cov2
   cov =  c(cov_m2, cov_m1, var, cov_1, cov_2)
   return (cov)
@@ -45,7 +46,7 @@ cov_omega_theta <-function(omega, theta){
 
 ########################################################################################################################################
 # Implied covariance structure under the original BPP model (no time aggregation)
-implied_cov_splurge <-function(params, taste, T) {
+implied_cov_splurge <-function(params, T) {
   
   # read in the parameters
   var_perm    <- params[1] 
@@ -56,11 +57,8 @@ implied_cov_splurge <-function(params, taste, T) {
   omega       <- params[6]
   theta       <- params[7]
   var_c_error <- params[8] 
-  if (taste) {
-    varcsi    <- params[9] }
-  else {
-    varcsi    <- 0.0
-  }
+  varcsi     <- params[9] 
+
   # Set up covariance matricies, initialized to zero
   cov_y  <- matrix(0,nrow=T,ncol=T) #/* Income */
   cov_c <- matrix(0,nrow=T,ncol=T) #/* Consumption w/o measurement error */
@@ -86,19 +84,19 @@ implied_cov_splurge <-function(params, taste, T) {
   cov_om_th = cov_omega_theta(omega, theta)
   cov_om_th_m2 = cov_om_th[1]
   cov_om_th_m1 = cov_om_th[2]
-  cov_om_th    = cov_om_th[3]
+  cov_om_th_0  = cov_om_th[3]
   cov_om_th_1  = cov_om_th[4]
   cov_om_th_2  = cov_om_th[5]
 
 
   #/* This is the covariance of Income */
   for (j in 1:T){
-    cov_y[j,j] = 2.0/3.0*var_perm 
-                  + var_tran*var_omega
+    cov_y[j,j] = 2.0/3.0*var_perm +
+                   var_tran*var_omega
   }
   for (j in 2:T){
-    cov_y[j-1,j] <- 1.0/6.0*var_perm 
-                    + var_tran*cov_omega_1
+    cov_y[j-1,j] <- 1.0/6.0*var_perm +
+                     var_tran*cov_omega_1
     cov_y[j,j-1] <- cov_y[j-1,j]
   }
   for (M in 2:(T-1)){
@@ -110,7 +108,7 @@ implied_cov_splurge <-function(params, taste, T) {
   #/* This is the covariance of consumption */
   for (j in 1:T){
     cov_c[j,j] <- 2.0/3.0*phi^2*var_perm + var_tran*(  psi_tilde^2       *var_omega 
-                                                     + 2.0*psi*psi_tilde *cov_om_th 
+                                                     + 2.0*psi*psi_tilde *cov_om_th_0 
                                                      + psi^2             *var_theta) + varcsi
   }
   for (j in 2:T){
@@ -141,14 +139,14 @@ implied_cov_splurge <-function(params, taste, T) {
   #/* This is the covariance of income and consumption */
   for (j in 1:T){
     cov_y_c[j,j] = 2.0/3.0*phi*var_perm + var_tran * (  psi_tilde *var_omega 
-                                                      + psi       *cov_om_th )
+                                                      + psi       *cov_om_th_0 )
   }
   for (j in 2:T){
-    cov_y_c[j-1,j] <- 1.0/6.0*phi*var_perm
-                        + var_tran * (  psi_tilde *cov_omega_1
+    cov_y_c[j-1,j] <- 1.0/6.0*phi*var_perm +
+                         var_tran * (  psi_tilde *cov_omega_1
                                       + psi       *cov_om_th_m1 )
-    cov_y_c[j,j-1] <- 1.0/6.0*phi*var_perm
-                        + var_tran * (  psi_tilde *cov_omega_1
+    cov_y_c[j,j-1] <- 1.0/6.0*phi*var_perm +
+                         var_tran * (  psi_tilde *cov_omega_1
                                       + psi       *cov_om_th_1 )
   }
   for (M in 2:(T-1)){
@@ -170,90 +168,116 @@ implied_cov_splurge <-function(params, taste, T) {
 }
 
 # Estimates splurge parameters
-splurge_parameter_estimation <- function(c_vector, Omega, T, taste=1){
+splurge_parameter_estimation <- function(c_vector, Omega, T, init_params=NULL, fixed_index=NULL,moments_for_estimation=NULL){
   
-  init_params <- matrix(0,nrow=8+taste,ncol=1)
-  init_params[1] <- 0.003  # var_perm
-  init_params[2] <- 0.003  # var_tran
-  init_params[3] <- 0.9    # phi
-  init_params[4] <- 0.3    # psi
-  init_params[5] <- 0.2    # psi_tilde
-  init_params[6] <- 5.0    # omega - exponential decay of transitory shock
-  init_params[7] <- 0.4    # theta - exponential decay of consumption response to transitory shock
-  init_params[8] <- 0.06   # var_c_error  
-  if (taste){
+  #set initial parameters if not given
+  if (is.null(init_params)){
+    init_params <- matrix(0,nrow=9,ncol=1)
+    init_params[1] <- 0.003  # var_perm
+    init_params[2] <- 0.003  # var_tran
+    init_params[3] <- 0.9    # phi
+    init_params[4] <- 0.3    # psi
+    init_params[5] <- 0.2    # psi_tilde
+    init_params[6] <- 5.0    # omega - exponential decay of transitory shock
+    init_params[7] <- 0.4    # theta - exponential decay of consumption response to transitory shock
+    init_params[8] <- 0.06   # var_c_error 
     init_params[9] <- 0.01 #variance of taste shocks
   }
   
-  objectiveFun <-function(params, taste, T, empirical_cov, weight_matrix){
-    model_cov <- implied_cov_splurge(params, taste, T)
+  #fix certain parameters if required
+  if (!is.null(fixed_index)) {
+      estimate_params = init_params[!fixed_index]
+      fixed_values = init_params[fixed_index]
+  } else {
+    estimate_params = init_params
+    fixed_index = matrix(FALSE,nrow=length(init_params),ncol=0)
+    fixed_values = matrix(0,nrow=0,ncol=0)
+  }
+
+  #Select moments used in the estimation
+  if (is.null(moments_for_estimation)){
+    moments_for_estimation = matrix(TRUE,nrow=length(c_vector),ncol=1)
+  }
+  c_vector_used = c_vector[moments_for_estimation]
+  Omega_used    = Omega[moments_for_estimation,moments_for_estimation]
+
+  implied_cov_splurge_varying_params_only <-function(estimate_params, T,fixed_index,fixed_values,moments_for_estimation) {
+    params = matrix(0,nrow=length(fixed_index),ncol=1)
+    params[!fixed_index] = estimate_params
+    params[fixed_index] = fixed_values
+    model_cov <- implied_cov_splurge(params, T)
+    model_cov_used = model_cov[moments_for_estimation]
+    return (model_cov_used)
+  }
+  objectiveFun <-function(estimate_params, T, empirical_cov, weight_matrix,fixed_index,fixed_values,moments_for_estimation){
+    model_cov <- implied_cov_splurge_varying_params_only(estimate_params, T,fixed_index,fixed_values,moments_for_estimation)
     distance <- (model_cov-empirical_cov) %*% weight_matrix %*% (model_cov-empirical_cov)
     return (distance)
   }
   
   # Define the weight matrix as Equal Weight Minimum Distance
-  weight_matrix <- diag(diag(Omega)^(-1))
-  weight_matrix <- diag(length(c_vector))
+  weight_matrix <- diag(diag(Omega_used)^(-1))
+  #weight_matrix <- diag(length(c_vector_used))
   
-  ret <- objectiveFun(init_params, taste, T, c_vector, weight_matrix)
+  ret <- objectiveFun(estimate_params, T, c_vector_used, weight_matrix,fixed_index,fixed_values,moments_for_estimation)
   
-  solved_objective <- nlm(objectiveFun, init_params, taste, T, c_vector, weight_matrix, iterlim = 1000)
+  solved_objective <- nlm(objectiveFun, estimate_params, T, c_vector_used, weight_matrix,fixed_index,fixed_values,moments_for_estimation, iterlim = 1000, print.level=2)
   solved_params <- solved_objective$estimate
-  jacob <- jacobian(implied_cov_splurge, solved_params, taste=taste,T=T)
+  jacob <- jacobian(implied_cov_splurge_varying_params_only, solved_params,T=T,fixed_index=fixed_index,fixed_values=fixed_values,moments_for_estimation=moments_for_estimation)
   
   Sandwich1 <- solve(t(jacob) %*% weight_matrix %*% jacob)
-  Sandwich2 <- t(jacob) %*% weight_matrix %*% Omega %*% weight_matrix %*% jacob
+  Sandwich2 <- t(jacob) %*% weight_matrix %*% Omega_used %*% weight_matrix %*% jacob
   cov_params <- Sandwich1 %*% Sandwich2 %*% Sandwich1
   standard_errors <- diag(cov_params)^0.5
   
-  # read solution
-  var_perm    <- solved_params[1] 
-  var_tran    <- solved_params[2]
-  phi         <- solved_params[3] 
-  psi         <- solved_params[4]
-  psi_tilde   <- solved_params[5]
-  omega       <- solved_params[6]
-  theta       <- solved_params[7]
-  var_c_error <- solved_params[8] 
-  if (taste) {
-    varcsi    <- solved_params[9] }
-  else {
-    varcsi    <- 0.0
-  }
-  # read standard errors
-  var_perm_se    <- standard_errors[1] 
-  var_tran_se    <- standard_errors[2]
-  phi_se         <- standard_errors[3] 
-  psi_se         <- standard_errors[4]
-  psi_tilde_se   <- standard_errors[5]
-  omega_se       <- standard_errors[6]
-  theta_se       <- standard_errors[7]
-  var_c_error_se <- standard_errors[8] 
-  if (taste) {
-    varcsi_se    <- standard_errors[9] }
-  else {
-    varcsi_se    <- 0.0
-  }
-
-  output = list("var_perm"=var_perm,
-                "var_perm_se"=var_perm_se,
-                "var_tran"=var_tran, 
-                "var_tran_se"=var_tran_se,
-                "phi"=phi, 
-                "phi_se"=phi_se, 
-                "psi"=psi, 
-                "psi_se"=psi_se, 
-                "psi_tilde"=psi_tilde, 
-                "psi_tilde_se"=psi_tilde_se, 
-                "omega"=omega, 
-                "omega_se"=omega_se, 
-                "theta"=theta, 
-                "theta_se"=theta_se, 
-                "var_c_error"=var_c_error,
-                "var_c_error_se"=var_c_error_se,
-                " varcsi"=varcsi, 
-                "varcsi_se"=varcsi_se)
-  return (output)
-}
+  output_params = matrix(0,nrow=length(fixed_index),ncol=1)
+  output_params[!fixed_index] = solved_params
+  output_params[fixed_index] = fixed_values
   
+  output_se = matrix(0,nrow=length(fixed_index),ncol=1)
+  output_se[!fixed_index] = standard_errors
+  output_se[fixed_index] = 0.0
+  
+  # read solution
+  var_perm    <- output_params[1] 
+  var_tran    <- output_params[2]
+  phi         <- output_params[3] 
+  psi         <- output_params[4]
+  psi_tilde   <- output_params[5]
+  omega       <- output_params[6]
+  theta       <- output_params[7]
+  var_c_error <- output_params[8] 
+  varcsi    <- output_params[9] 
+
+  # read standard errors
+  var_perm_se    <- output_se[1] 
+  var_tran_se    <- output_se[2]
+  phi_se         <- output_se[3] 
+  psi_se         <- output_se[4]
+  psi_tilde_se   <- output_se[5]
+  omega_se       <- output_se[6]
+  theta_se       <- output_se[7]
+  var_c_error_se <- output_se[8] 
+  varcsi_se      <- output_se[9] 
+
+  output = list("var_perm"        =var_perm,
+                 "var_tran"       =var_tran, 
+                "phi"             =phi, 
+                "psi"             =psi, 
+                "psi_tilde"       =psi_tilde, 
+                "omega"           =omega, 
+                "theta"           =theta, 
+                "var_c_error"     =var_c_error,
+                "varcsi"          =varcsi, 
+                "var_perm_se"     =var_perm_se,
+                "var_tran_se"     =var_tran_se, 
+                "phi_se"          =phi_se, 
+                "psi_se"          =psi_se, 
+                "psi_tilde_se"    =psi_tilde_se, 
+                "omega_se"        =omega_se, 
+                "theta_se"        =theta_se, 
+                "var_c_error_se"  =var_c_error_se,
+                "varcsi_se"       =varcsi_se)
+  return (output) 
+  }
   
--- Code/Run_estimate_splurge.R
+++ Code/Run_estimate_splurge.R
@@ -12,5 +12,76 @@ c_vector = as.vector(t(read.csv(file=paste(moments_BPP_dir,"/moments_all_c_vecto
 Omega =    as.matrix(read.csv(file=paste(moments_BPP_dir,"/moments_all_omega.txt",    sep=""), header=FALSE, sep=","))
 T=12
 
+init_params <- matrix(0,nrow=9,ncol=1)
+init_params[1] <- 0.01  # var_perm
+init_params[2] <- 0.003  # var_tran
+init_params[3] <- 0.8    # phi
+init_params[4] <- 0.5    # psi
+init_params[5] <- 0.05    # psi_tilde
+init_params[6] <- 6.0    # omega - exponential decay of transitory shock
+init_params[7] <- 0.5    # theta - exponential decay of consumption response to transitory shock
+init_params[8] <- 0.06   # var_c_error  
+init_params[9] <- 0.00 #variance of taste shocks
+
+
+fixed_index = c(FALSE,      # var_perm
+                FALSE,      # var_tran
+                TRUE,      # phi
+                TRUE,      # psi
+                TRUE,      # psi_tilde
+                FALSE,      # omega
+                TRUE,      # theta
+                TRUE,      # var_c_error
+                TRUE)     # taste shocks
+
+# select which moments to use for estimation
+use_c_cov = FALSE
+use_y_cov = TRUE
+use_yc_cov = FALSE
+moments_for_estimation_matrix = matrix(TRUE,nrow=2*T,ncol=2*T)
+moments_for_estimation_matrix[1:T,1:T]                 <-   matrix(use_c_cov,nrow=T,ncol=T)
+moments_for_estimation_matrix[(T+1):(2*T),1:T]         <-   matrix(use_yc_cov,nrow=T,ncol=T)
+moments_for_estimation_matrix[1:T,(T+1):(2*T)]         <-   matrix(use_yc_cov,nrow=T,ncol=T)
+moments_for_estimation_matrix[(T+1):(2*T),(T+1):(2*T)] <-   matrix(use_y_cov,nrow=T,ncol=T)
+moments_for_estimation = vech(moments_for_estimation_matrix)
+
 #Next replicate BPP
-BPP_output = splurge_parameter_estimation(c_vector, Omega, T, taste=1) 
+BPP_output = splurge_parameter_estimation(c_vector, Omega, T, init_params=init_params,fixed_index=fixed_index,moments_for_estimation=moments_for_estimation) 
+
+
+########################################################
+###############################################################################
+# Function to estimate parameters for each category for which we have moments
+estimation_splurge_by_category<- function(moments_BPP_dir,moments_stub,category_set, T=12, init_params,fixed_index,moments_for_estimation) {
+  category_params = array(0, dim=c(length(category_set),5))
+  category_se = array(0, dim=c(length(category_set),5))
+  for (i in 1:length(category_set)){
+    this_category = as.character(category_set[i])
+    this_c_vector = as.vector(t(read.csv(file=paste(moments_BPP_dir,"/",moments_stub,i,"c_vector.txt", sep=""), header=FALSE, sep=",")))
+    this_omega = as.matrix(read.csv(file=paste(moments_BPP_dir,"/",moments_stub,i,"_omega.txt", sep=""), header=FALSE, sep=","))
+    this_output = splurge_parameter_estimation(this_c_vector, this_omega,T, init_params=init_params,fixed_index=fixed_index,moments_for_estimation=moments_for_estimation) 
+    category_params[i,1] = this_output$var_perm
+    category_params[i,2] = this_output$var_tran
+    category_params[i,3] = this_output$phi
+    category_params[i,4] = this_output$psi
+    category_params[i,5] = this_output$psi_tilde
+    category_se[i,1]     = this_output$var_perm_se
+    category_se[i,2]     = this_output$var_tran_se
+    category_se[i,3]     = this_output$phi_se
+    category_se[i,4]     = this_output$psi_se
+    category_se[i,5]    = this_output$psi_tilde_se
+  }
+  output = list("category_params"=category_params,"category_se"=category_se)
+  return (output)
+}
+
+###############################################################################
+# load liquid weath quintile data and create graph
+moments_stub = "moments_by_liquid_wealth_quantile"
+num_quantiles = 5
+round_digits = -3
+wealth_quantile_set = as.character(1:num_quantiles)
+output =estimation_splurge_by_category(moments_BPP_dir,moments_stub, make.names(wealth_quantile_set), T=12,init_params=init_params,fixed_index=fixed_index,moments_for_estimation=moments_for_estimation) 
+wealth_quantile_output=output
+wealth_quantile_params = output$category_params
+wealth_quantile_se = output$category_se
