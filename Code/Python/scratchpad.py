Fixes to machine precision work + completed tests!

Code/Python/scratchpad.py
@@ -1,5 +1,6 @@
 
 import numpy as np
+import pandas as pd
 
 # calculates exp(x) -1 - x to high precision
 def expm1mx(x):
@@ -24,32 +25,7 @@ def expm1mxm05x2(x):
                 n_factorial *= (j+1)
             ret += x**n/n_factorial
     return ret
-
-
-def cov_omega_theta(omega, theta):
-  omegax = -1.0/np.expm1(-omega)
-  thetax = -1.0/np.expm1(-theta)
-  omth = omega + theta
-  omthx = -1.0/np.expm1(-omth)
-  #variance at T
-  var = 2.0*thetax*omegax  \
-          + thetax*omegax*(  ((2.0-np.exp(-theta))*(2.0-np.exp(-omega))+1.0)/(omth*omthx)     \
-                           - (3.0-np.exp(-theta))/(theta*thetax) - (3.0-np.exp(-omega))/(omega*omegax)   )  \
-            +1.0/(omth*omegax*thetax)
-  # Covariance for moving the theta process up to T+1
-  cov_1 = thetax*omegax*((2.0-np.exp(-theta))/(theta*thetax)    \
-                          + 1.0/(omega*omegax) - (2.0-np.exp(-theta))/(omth*omthx)  \
-                          -1.0 )    \
-           - omegax/thetax*((2.0-np.exp(-omega))/(omth*omthx) - 1.0/(theta*thetax) )     \
-           + np.exp(-theta)/(omegax*thetax*omth)
-
-  # Covariance for moving the theta process up to T+2
-  cov_2 = -omegax/thetax * (1.0/(theta*thetax) - 1.0/(omth*omthx))  \
-          -omegax/thetax * (np.exp(-theta)*(2.0-np.exp(-omega))/(omth*omthx) - np.exp(-theta)/(theta*thetax) ) \
-          +np.exp(-2.0*theta)/(omegax*thetax*omth)
-  return np.array([var, cov_1, cov_2])
-
-            
+           
 def cov_omth_test(omega, theta):
   '''
     Calculates the covariance of two time aggregated exponential processes, 
@@ -76,12 +52,12 @@ def cov_omth_test(omega, theta):
       if (omega==0.0):
           #variance at T
           var_T0 = -1/(expm1_th*theta**2)*(expm1mxm05x2_th + expm1mx_th*theta )
-          var_T1 = -np.exp(-theta)/expm1_th*( expm1mx_th/theta + 1/theta**2*(expm1mxm05x2_th+theta*expm1mx_th) + 0.5*expm1_th )
+          var_T1 = - (1-expm1_th)/expm1_th*( -expm1_th/theta +1/theta**2*(expm1mxm05x2_th + expm1mx_th*theta) -0.5  -0.5/(1-expm1_th))
           var_Tinf = 0.0
           var = var_T0 + var_T1 + var_Tinf
           # Covariance for moving the theta process up to T+1
           cov_1_T0 = -1/expm1_th *( (1-expm1_th)/theta**2*(1-np.exp(-theta) - theta*np.exp(-theta)) -0.5)
-          cov_1_T1  = 0.5-(1-expm1_th)/(expm1_th*theta**2)*( - expm1mxm05x2_th - theta*expm1mx_th )
+          cov_1_T1  = expm1_th*( -expm1_th/theta -0.5 +1/theta**2*(expm1mxm05x2_th + expm1mx_th*theta)) 
           cov_1_Tinf = 0.0
           cov_1 = cov_1_T0 + cov_1_T1 + cov_1_Tinf
           # Covariance for moving the theta process up to T+2
@@ -98,7 +74,13 @@ def cov_omth_test(omega, theta):
           # Covariance for moving the omega process up to T+2
           cov_m2 = 0.0
       elif (theta==0.0):
-          return cov_omth_test(theta, omega)
+          ######Need to replace this when I rename the funtion
+          reverse_return = cov_omth_test(theta, omega)
+          var = reverse_return[2]
+          cov_1 = reverse_return[1]
+          cov_2 = reverse_return[0]
+          cov_m1 = reverse_return[3]
+          cov_m2 = reverse_return[4]
       else:        
           #variance at T
           var_T0 = 1.0/(expm1_om*expm1_th)*(              expm1mxm05x2_om/omega +              expm1mxm05x2_th/theta -                           expm1mxm05x2_omth/omth)
@@ -129,34 +111,35 @@ def cov_omth_test(omega, theta):
   return np.array([cov_m2, cov_m1, var, cov_1, cov_2])
 
 omega = 0.13
-theta = 0.09
+theta = 0.3
 x1=cov_omega_theta(omega,theta)
 x2=cov_omth_test(omega,theta)
 xdiff=cov_omega_theta(omega,theta)-cov_omth_test(omega,theta)
-[x1,x2,xdiff]
+pd.DataFrame([x1,x2,xdiff])
 
 
-omega = 0.000000000000001
-theta = 0.09
-x1=cov_omega_theta(omega,theta)
+omega = 0.00000001
+theta = 0.9
+x1=cov_omth_test(0.0,theta)
 x2=cov_omth_test(omega,theta)
-xdiff=cov_omega_theta(omega,theta)-cov_omth_test(omega,theta)
-[x1,x2,xdiff]
+xdiff=x2-x1
+pd.DataFrame([x1,x2,xdiff])
 
-omega = 0.000000000000001
-theta = 0.0000000000000001
-x1=cov_omega_theta(omega,theta)
+omega = 0.3
+theta = 0.0000001
+x1=cov_omth_test(omega,0.0)
 x2=cov_omth_test(omega,theta)
-xdiff=cov_omega_theta(omega,theta)-cov_omth_test(omega,theta)
-[x1,x2,xdiff]
+xdiff=x2-x1
+pd.DataFrame([x1,x2,xdiff])
+
 
 
 omega = 0.0000000000000001
-theta = 0.000000000000001
-x1=cov_omega_theta(omega,theta)
+theta = 0.0000000000000001
+x1=cov_omth_test(0.0,0.0)
 x2=cov_omth_test(omega,theta)
-xdiff=cov_omega_theta(omega,theta)-cov_omth_test(omega,theta)
-[x1,x2,xdiff]
+xdiff=x1-x2
+pd.DataFrame([x1,x2,xdiff])
 
 
 